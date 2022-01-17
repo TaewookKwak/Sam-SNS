d8a67d421d590af0a160472b8ff015d83aacce66
Taewook <rhkrxodnr@gmail.com>
2022-01-17 21:42:12 +0900

    add firebase

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <p className="App">Hello</p>
+}
+
+export default App
---
+++ src/lib/firebase.js
@@ -0,0 +1,22 @@
+import { FirebaseError } from '@firebase/util'
+import Firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+
+// import { seedDatabase } from '../seed'
+
+const config = {
+  apiKey: 'AIzaSyDo6jlMCvBwIJS95qzSXYIb8WOT8BztKLQ',
+  authDomain: 'sam-sns.firebaseapp.com',
+  projectId: 'sam-sns',
+  storageBucket: 'sam-sns.appspot.com',
+  messagingSenderId: '293494173326',
+  appId: '1:293494173326:web:3ed409d1449d8b95ab63df',
+}
+const firebase = Firebase.initializeApp(config)
+const { FieldValue } = Firebase.firestore
+
+// seedDatabase(firebase)
+console.log('firebase', firebase)
+
+export { firebase, FieldValue }
