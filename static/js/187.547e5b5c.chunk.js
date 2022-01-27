92aa120d26a41dbe1176b54425a2911a63053433
Taewook <rhkrxodnr@gmail.com>
2022-01-27 22:31:13 +0900

    Updates

---
+++ static/js/187.547e5b5c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinstaram=self.webpackChunkinstaram||[]).push([[187],{6187:function(n,t,e){e.r(t);var s=e(2791),u=e(5544),i=e(184);t.default=function(){return(0,s.useEffect)((function(){document.title="Not Found - Sams SNS"}),[]),(0,i.jsxs)("div",{children:[(0,i.jsx)(u.Z,{}),(0,i.jsx)("div",{children:"NOT FOUND..."})]})}}}]);
+//# sourceMappingURL=187.547e5b5c.chunk.js.map
